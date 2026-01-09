a7fae980e511af835fda6134233559715d175e84
Siron <sironmudalige@gmail.com>
2026-01-09 13:40:27 +0530

    Added code to backup devops-lab file

---
+++ script1.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+SOURCE_DIR="$HOME/devops-lab"
+BACKUP_DIR="$HOME/backups"
+
+mkdir -p "$BACKUP_DIR"
+
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_FILE="BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
+
+tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
+
+if [ $? -eq 0]; then
+  echo "Backup successful: $BACKUP_FILE"
+else
+  echo "Backup failed"
+fi
