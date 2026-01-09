12b5fc59968b1d119ae734be3db3d2a89f3f66b0
Siron <sironmudalige@gmail.com>
2026-01-09 13:51:53 +0530

    correct BACKUP_DIR mistake and [ 0 -eq 0 ] mistake

--- script1.sh
+++ script1.sh
@@ -6,11 +6,11 @@ BACKUP_DIR="$HOME/backups"
 mkdir -p "$BACKUP_DIR"
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-BACKUP_FILE="BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
+BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
 
 tar -czf "$BACKUP_FILE" "$SOURCE_DIR"
 
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo "Backup successful: $BACKUP_FILE"
 else
   echo "Backup failed"
